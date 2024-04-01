9d5dd8dc156a2e3a2ea49d6c453935c33eda00d3
Heath <heathcliffdogar@gmail.com>
2024-04-01 19:07:31 +0200

    almost finished with norminette

--- source/game.c
+++ source/game.c
@@ -6,33 +6,11 @@
 /*   By: hdorado- <hdorado-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 14:49:09 by hdorado-          #+#    #+#             */
-/*   Updated: 2024/04/01 18:25:41 by hdorado-         ###   ########.fr       */
+/*   Updated: 2024/04/01 19:04:57 by hdorado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
-
-// void	ft_fake_game(t_map *game)
-// {
-// 	char	**arr;
-
-// 	arr = ft_calloc(12, sizeof(char *));
-// 	int i = -1;
-// 	while (++i < 11)
-// 		arr[i] = ft_calloc(sizeof(char), 11);
-// 	arr[0] = "1111111111";
-// 	arr[1] = "1000000001";
-// 	arr[2] = "1000000001";
-// 	arr[3] = "1000000001";
-// 	arr[4] = "1000000001";
-// 	arr[5] = "1000000001";
-// 	arr[6] = "1000000001";
-// 	arr[7] = "1000000001";
-// 	arr[8] = "1000000001";
-// 	arr[9] = "1111111111";
-// 	arr[10] = 0;
-// 	game->matrix = arr;
-// }
 
 uint32_t	ft_get_pixel(mlx_image_t *image, double horizontal, double vertical)
 {
@@ -42,11 +20,10 @@ uint32_t	ft_get_pixel(mlx_image_t *image, double horizontal, double vertical)
 
 	color = 0;
 	if (horizontal >= 0)
-		x = (int) (95 * horizontal); //95 is because the image is 96x96
+		x = (int)(95 * horizontal);
 	else
-		x = (int) (95 * (1.0 + horizontal));
-	y = (int) (95 * vertical);
-	//printf("In the wall, print value (%d, %d) from (%f, %f)\n", x, y, horizontal, vertical);
+		x = (int)(95 * (1.0 + horizontal));
+	y = (int)(95 * vertical);
 	color += image->pixels[(y * 96 + x) * 4] << 24;
 	color += image->pixels[(y * 96 + x) * 4 + 1] << 16;
 	color += image->pixels[(y * 96 + x) * 4 + 2] << 8;
@@ -54,115 +31,123 @@ uint32_t	ft_get_pixel(mlx_image_t *image, double horizontal, double vertical)
 	return (color);
 }
 
+typedef struct s_raycast
+{
+	t_vector	ray;
+	t_vector	d_dist;
+	t_vector	s_dist;
+	double		section;
+	int			mapx;
+	int			mapy;
+	int			stepx;
+	int			stepy;
+	int			hit;
+	int			side;
+	double		walldist;
+	int			lineheight;
+	int			drawstart;
+	int			drawend;
+}	t_raycast;
+
 void	ft_raycast(void *param)
 {
-	double	i;
-	t_map	*game;
-	t_vector	ray;
-	double		section;
+	double		i;
+	t_map		*game;
+	t_raycast	r;
 	uint32_t	hex_color;
+	int			j;
 
 	game = (t_map *) param;
 	i = -1.0;
 	render_floor_ceiling(game);
-	while (++i < 960) //For each (vertical column of) pixel in the screen
+	while (++i < 960)
 	{
-		section = (2*i/959) - 1; //(959 because of 0), this will select each column
-		ray.x = game->player->dir.x + game->player->camera.x*section; //Create the direction of the "ray of light", x coordinate
-		ray.y = game->player->dir.y + game->player->camera.y*section; //Create the direction of the "ray of light", y coordinate
-		int	mapX = (int)game->player->pos.x;
-		int mapY = (int)game->player->pos.y;
-		//Now we need to find how far away is the wall. This has two components, the sideDist (distance from current position until first border) and deltaDist (distance from first border to the next)
-		double deltaDistX; //Find out the distance the ray needs to travel so it finds the next wall. If ray x or y is 0, divide by 1e30 to avoid division by 0
-		if (ray.x == 0)
-			deltaDistX = fabs(1/1e-30);
+		r.section = (2 * i / 959) - 1;
+		r.ray.x = game->player->dir.x + game->player->camera.x * r.section;
+		r.ray.y = game->player->dir.y + game->player->camera.y * r.section;
+		r.mapx = (int)game->player->pos.x;
+		r.mapy = (int)game->player->pos.y;
+		if (r.ray.x == 0)
+			r.d_dist.x = fabs(1 / 1e-30);
 		else
-			deltaDistX = fabs(1/ray.x);
-		double deltaDistY;
-		if (ray.y == 0)
-			deltaDistY = fabs(1/1e-30);
+			r.d_dist.x = fabs(1 / r.ray.x);
+		if (r.ray.y == 0)
+			r.d_dist.y = fabs(1 / 1e-30);
 		else
-			deltaDistY = fabs(1/ray.y);
-		int stepX; //Are we walking in the positive or negative direction?
-		int	stepY;
-		int	hit = 0; //was there a wall hit?
-		int	side; //was a NS (0) or a EW (1) wall hit?
-		double	sideDistX;
-		double sideDistY;
-		if (ray.x < 0)
+			r.d_dist.y = fabs(1 / r.ray.y);
+		if (r.ray.x < 0)
 		{
-			stepX = -1;
-			sideDistX = (game->player->pos.x - mapX) * deltaDistX;
+			r.stepx = -1;
+			r.s_dist.x = (game->player->pos.x - r.mapx) * r.d_dist.x;
 		}
 		else
 		{
-			stepX = 1;
-			sideDistX = (mapX + 1.0 - game->player->pos.x) * deltaDistX;
+			r.stepx = 1;
+			r.s_dist.x = (r.mapx + 1.0 - game->player->pos.x) * r.d_dist.x;
 		}
-		if (ray.y < 0)
+		if (r.ray.y < 0)
 		{
-			stepY = -1;
-			sideDistY = (game->player->pos.y - mapY) * deltaDistY;
+			r.stepy = -1;
+			r.s_dist.y = (game->player->pos.y - r.mapy) * r.d_dist.y;
 		}
 		else
 		{
-			stepY = 1;
-			sideDistY = (mapY + 1.0 - game->player->pos.y) * deltaDistY;
+			r.stepy = 1;
+			r.s_dist.y = (r.mapy + 1.0 - game->player->pos.y) * r.d_dist.y;
 		}
-		while (!hit)
+		r.hit = 0;
+		while (!r.hit)
 		{
-			if (sideDistX < sideDistY)
+			if (r.s_dist.x < r.s_dist.y)
 			{
-				sideDistX += deltaDistX;
-				mapX += stepX;
-				side = 0;
+				r.s_dist.x += r.d_dist.x;
+				r.mapx += r.stepx;
+				r.side = 0;
 			}
 			else
 			{
-				sideDistY += deltaDistY;
-				mapY += stepY;
-				side = 1;
+				r.s_dist.y += r.d_dist.y;
+				r.mapy += r.stepy;
+				r.side = 1;
 			}
-			if (game->matrix[mapY][mapX] == '1')
-				hit = 1;
+			if (game->matrix[r.mapy][r.mapx] == '1')
+				r.hit = 1;
 		}
-		double WallDist;
-		if(side == 0)
-			WallDist = (sideDistX - deltaDistX);
+		if (r.side == 0)
+			r.walldist = (r.s_dist.x - r.d_dist.x);
 		else
-			WallDist = (sideDistY - deltaDistY);
-		int lineHeight;
-		if (WallDist <= 0.001)
-			lineHeight = (int) (600 / 0.001);
+			r.walldist = (r.s_dist.y - r.d_dist.y);
+		if (r.walldist <= 0.001)
+			r.lineheight = (int)(600 / 0.001);
 		else
-			lineHeight = (int)(600/ WallDist);
-		int drawStart = -lineHeight / 2 + 600 / 2;
-		if(drawStart < 0)
-			drawStart = 0;
-		int drawEnd = lineHeight / 2 + 600 / 2;
-		if(drawEnd >= 600)
-			drawEnd = 600 - 1;
-		int	j = drawStart;
-		while (j <= drawEnd)
+			r.lineheight = (int)(600 / r.walldist);
+		r.drawstart = -r.lineheight / 2 + 600 / 2;
+		r.drawend = r.lineheight / 2 + 600 / 2;
+		if (r.drawstart < 0)
+			r.drawstart = 0;
+		if (r.drawend >= 600)
+			r.drawend = 600 - 1;
+		j = r.drawstart;
+		while (j <= r.drawend)
 		{
-			if (side && (int) mapY < (int) game->player->pos.y)
+			if (r.side && (int) r.mapy < (int) game->player->pos.y)
 			{
-				hex_color = ft_get_pixel(game->graphics->img_wall_N, (ray.x * WallDist + game->player->pos.x - ((int)(ray.x * WallDist + game->player->pos.x))), ((double)(((lineHeight - 600) / 2) + j)) / ((double)(lineHeight)));
+				hex_color = ft_get_pixel(game->graphics->img_wall_n, (r.ray.x * r.walldist + game->player->pos.x - ((int)(r.ray.x * r.walldist + game->player->pos.x))), ((double)(((r.lineheight - 600) / 2) + j)) / ((double)(r.lineheight)));
 				mlx_put_pixel(game->w_id, (int) i, j, hex_color);
 			}
-			else if (side && (int) mapY > (int) game->player->pos.y)
+			else if (r.side && (int) r.mapy > (int) game->player->pos.y)
 			{
-				hex_color = ft_get_pixel(game->graphics->img_wall_S, (ray.x * WallDist + game->player->pos.x - ((int)(ray.x * WallDist + game->player->pos.x))), ((double)(((lineHeight - 600) / 2) + j)) / ((double)(lineHeight)));
+				hex_color = ft_get_pixel(game->graphics->img_wall_s, (r.ray.x * r.walldist + game->player->pos.x - ((int)(r.ray.x * r.walldist + game->player->pos.x))), ((double)(((r.lineheight - 600) / 2) + j)) / ((double)(r.lineheight)));
 				mlx_put_pixel(game->w_id, (int) i, j, hex_color);
 			}
-			else if (!side && (int) mapX > (int) game->player->pos.x)
+			else if (!r.side && (int) r.mapx > (int) game->player->pos.x)
 			{
-				hex_color = ft_get_pixel(game->graphics->img_wall_E, (ray.y * WallDist + game->player->pos.y - ((int)(ray.y * WallDist + game->player->pos.y))), ((double)(((lineHeight - 600) / 2) + j)) / ((double)(lineHeight)));
+				hex_color = ft_get_pixel(game->graphics->img_wall_e, (r.ray.y * r.walldist + game->player->pos.y - ((int)(r.ray.y * r.walldist + game->player->pos.y))), ((double)(((r.lineheight - 600) / 2) + j)) / ((double)(r.lineheight)));
 				mlx_put_pixel(game->w_id, (int) i, j, hex_color);
 			}
 			else
 			{
-				hex_color = ft_get_pixel(game->graphics->img_wall_W, (ray.y * WallDist + game->player->pos.y - ((int)(ray.y * WallDist + game->player->pos.y))), ((double)(((lineHeight - 600) / 2) + j)) / ((double)(lineHeight)));
+				hex_color = ft_get_pixel(game->graphics->img_wall_w, (r.ray.y * r.walldist + game->player->pos.y - ((int)(r.ray.y * r.walldist + game->player->pos.y))), ((double)(((r.lineheight - 600) / 2) + j)) / ((double)(r.lineheight)));
 				mlx_put_pixel(game->w_id, (int) i, j, hex_color);
 			}
 			j++;
@@ -224,20 +209,14 @@ void	move_player(t_map *game, char dir)
 		tmp = ft_fill_vector(game->player->pos.x + 0.2 * game->player->dir.x,
 				game->player->pos.y + 0.2 * game->player->dir.y);
 	else if (dir == 'S')
-	{
-		tmp.x = game->player->pos.x - 0.2 * game->player->dir.x;
-		tmp.y = game->player->pos.y - 0.2 * game->player->dir.y;
-	}
+		tmp = ft_fill_vector(game->player->pos.x - 0.2 * game->player->dir.x,
+				game->player->pos.y - 0.2 * game->player->dir.y);
 	else if (dir == 'E')
-	{
-		tmp.x = game->player->pos.x - 0.2 * game->player->dir.y;
-		tmp.y = game->player->pos.y + 0.2 * game->player->dir.x;
-	}
+		tmp = ft_fill_vector(game->player->pos.x - 0.2 * game->player->dir.y,
+				game->player->pos.y + 0.2 * game->player->dir.x);
 	else if (dir == 'W')
-	{
-		tmp.x = game->player->pos.x + 0.2 * game->player->dir.y;
-		tmp.y = game->player->pos.y - 0.2 * game->player->dir.x;
-	}
+		tmp = ft_fill_vector(game->player->pos.x + 0.2 * game->player->dir.y,
+				game->player->pos.y - 0.2 * game->player->dir.x);
 	else
 		tmp = ft_fill_vector(game->player->pos.x, game->player->pos.y);
 	if (ft_wall_dist(game, tmp))
